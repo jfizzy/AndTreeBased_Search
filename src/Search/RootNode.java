114cea19a5054214ee2a2b99d25c5c4e67273107
James MacIsaac <james@jmproj.com>
2017-11-26 20:07:47 -0700

    Moved TreeNode to Node and created some subclasses from it

---
+++ src/Search/RootNode.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Search;
+
+import Schedule.Schedule;
+
+/**
+ *
+ * @author
+ */
+public class RootNode extends Node {
+
+    public RootNode(Schedule s) {
+        super(s);
+    }
+
+    // TODO add some root specific functions here
+}
