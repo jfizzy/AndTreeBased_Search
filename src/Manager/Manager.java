ede2a6f6d1337c9c237e9c6be3a408832353ab10
lukeatmycode <luke.kissick1@ucalgary.ca>
2017-12-01 13:51:53 -0700

    cleaned up usage message

--- src/Manager/Manager.java
+++ src/Manager/Manager.java
@@ -33,7 +33,7 @@ class Manager {
      */
     public static void main(String[] args) {
 
-        // one argument or 5
+        // one argument or 6 (the file name and then 5 weights) 
         try {
             if (args.length == 1 || args.length == 6) {
 
@@ -74,7 +74,7 @@ class Manager {
     }
 
     private static void usage() { // TODO make this clearer
-        System.out.println("Acceptable args are: inputfile [weight] [weight] [weight] [weight]");
+        System.out.println("Acceptable args are: inputfile [CourseMin weight] [LabMin weight] [Pref weight] [Pair weight]  [Diff weight]");
         exit();
     }
 
